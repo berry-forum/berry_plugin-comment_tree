118e05967aa7aafee319761ee550a8554d681a55
SuperSonic <supersonic@livemail.tw>
2020-12-26 00:28:29 +0800

    Update

--- extend.php
+++ extend.php
@@ -30,7 +30,6 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js')
         ->css(__DIR__ . '/resources/less/forum.less'),
-    new Extend\Locales(__DIR__ . '/resources/locale'),
     (new Extend\Routes('api'))
         ->get('/trees/{id}', 'evergeen.trees.get', ListTreePostController::class),
     (new Extend\Event())
